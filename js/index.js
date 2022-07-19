d864669151aec4b15ef5767bc3df9976b76375d4
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-19 19:49:13 +0200

    add iteration 1 (names and input)

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "John";
+let hacker2 = "Mary";
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The driver's name is ${hacker2}`);
+
+
 // Iteration 2: Conditionals
 
 
