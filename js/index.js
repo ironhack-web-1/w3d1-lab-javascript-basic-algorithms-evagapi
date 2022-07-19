574b1bc231aabc40896d4613e97092d81462039d
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-07-19 20:06:30 +0200

    feat: add iteration 2 (conditionals)

--- js/index.js
+++ js/index.js
@@ -9,7 +9,17 @@ console.log(`The driver's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+function isLarger(driver, navigator) {
+  if (driver.length > navigator.length) {
+    console.log(`The driver has the longest name, it has ${driver.length} characters.`);
+  } else if (navigator.length > driver.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`);
+  } else if (driver.length === navigator.length) {
+    console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+  }
+};
 
 
+console.log(isLarger(hacker1, hacker2));
 
 // Iteration 3: Loops
